9eabcf141318792bcbfd8151b21ffd20d37cf50d
Phil Taylor <phil@m0vse.uk>
2025-10-16 22:45:28 +0000

    Fix duplicate

--- lib/SNMP/Info.pm
+++ lib/SNMP/Info.pm
@@ -1877,7 +1877,6 @@ sub device_type {
         171   => 'SNMP::Info::Layer3::DLink',
         207   => 'SNMP::Info::Layer2::Allied',
 	248   => 'SNMP::Info::Layer2::Hirschmann',
-        248   => 'SNMP::Info::Layer2::Hirschmann',
         266   => 'SNMP::Info::Layer2::Nexans',
         664   => 'SNMP::Info::Layer2::Adtran',
         674   => 'SNMP::Info::Layer3::Dell',
